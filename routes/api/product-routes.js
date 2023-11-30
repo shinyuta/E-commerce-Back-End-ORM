6d25fd0c6189d70be4a64c3dcf2612bb8693eb43
Yuta Kirihara <116908432+shinyuta@users.noreply.github.com>
2023-11-30 17:09:35 -0600

    fixed product delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -135,21 +135,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+// deletes by id
+router.delete('/:id', async (req, res) => {
   try {
     const productData = await Product.destroy({
       where: {
-        id: reg.params.id,
+        id: req.params.id,
       },
-      if (!productData) {
-        res.status(404).json({ message: 'No such product found! });
-          return;
-      }
-      res.status(200).json(productData);
-      } catch (err) {
-        res.status(500).json(err);
-      }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No such product found!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
