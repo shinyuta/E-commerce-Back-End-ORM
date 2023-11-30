dfb156857f710c0726e2b669533d2d6f6ad8ad05
Yuta Kirihara <116908432+shinyuta@users.noreply.github.com>
2023-11-30 17:07:38 -0600

    added productData delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -137,6 +137,19 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: reg.params.id,
+      },
+      if (!productData) {
+        res.status(404).json({ message: 'No such product found! });
+          return;
+      }
+      res.status(200).json(productData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 module.exports = router;
